89dcbca511241ff8f2acd84f1d0b24a289519c9b
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-03-09 10:40:08 +0100

    start

---
+++ objectorienteret.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+canvas.style.background = "white";
+canvas.width = 1000;
+canvas.height = 550;
+let player1 =new player(100,100,10,"red")
+player1.make();
