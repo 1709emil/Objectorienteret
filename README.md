a5388a69526740205e0b8c0c32d25c9dc5d8e001
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-03-12 16:34:20 +0100

    Changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Objectorienteret
 
-Jeg vil gerne prøve at lave en slags spiller man kan bevæge med arrow keys
+Jeg vil gerne prøve at lave en slags pause background hvor der et object der bevæger sig 
--- objectorienteret.js
+++ objectorienteret.js
@@ -5,11 +5,14 @@ canvas.width = 1000;
 canvas.height = 550;
 
 class player {
-  constructor(posY,posX,radius,color) {
+  constructor(posY,posX,radius,color,speed) {
     this.posY=posY;
     this.posX=posX;
     this.color=color;
     this.radius=radius;
+    this.speed = speed;
+    this.difx= 1 * this.speed;
+    this.dify= 1 * this.speed;
   }
  draw(context){
 context.beginPath();
@@ -19,7 +22,36 @@ context.arc(this.posX,
   0,
   Math.PI *2);
 context.fillStyle=this.color;
-context.fill();}
+context.fill();context.closePath();}
+
+
+update(){
+context.clearRect(0,0,1000,550);
+this.draw(context);
+if( (this.posX + this.radius) >canvas.width){
+  this.difx = -this.difx;}
+
+if( (this.posX - this.radius) < 0){
+    this.difx = -this.difx; }
+
+if( (this.posY - this.radius) >canvas.height){
+    this.dify = -this.dify;}
+
+if( (this.posY + this.radius) < 0){
+    this.dify = -this.dify;}
+
+this.posX +=+ this.difx;
+this.posY +=+ this.dify;
+
 }
-let player1 = new player(100,100,10,"red");
+}
+
+
+let player1 = new player(100,100,10,"red",1);
 player1.draw(context);
+
+let updatePlayerPos = function() {
+  requestAnimationFrame(updatePlayerPos);
+  player1.update();
+}
+updatePlayerPos();
